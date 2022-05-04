a2307bf6c0c36374b55ae11e6e534e6e4fb34960
Mike Lawrence <mjalawrence1@gmail.com>
2022-05-04 10:26:29 +0100

    the liiiiine

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,8 +4,8 @@ import ChangeRestaurantButton from "../ChangeRestaurantButton";
 const Header = ({restaurantID, idNameResetter}) => {
     return(
         <>
-            <div className='container'>
                 <div className='App-header'>
+            <div className='header-container'>
                     <p><span className= 'blue-text'> Food</span>Delivery</p>
                     {restaurantID ? <ChangeRestaurantButton idNameResetter={idNameResetter} /> : ''}
                 </div>
