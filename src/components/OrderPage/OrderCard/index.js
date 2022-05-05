75bd4780a51f2b9072422dded7711521b389f565
Ali McGarry <ali.mcgarry@gmail.com>
2022-05-05 11:10:40 +0100

    Working on order items in order card

--- src/components/OrderPage/OrderCard/index.js
+++ src/components/OrderPage/OrderCard/index.js
@@ -1,9 +1,19 @@
+import QuantityButtons from '../QuantityButtons'
 import './OrderCard.scss'
-const OrderCard = () => {
+const OrderCard = ({ orderObject}) => {
+    const generateOrdersMarkup = () => {
+        // Loop through the orderObject
+        // For each item in the order object you add name and QuantityButtons component
+        // You return the ordersMarkup JSX
+    }
+
+    const orderMarkup = generateOrdersMarkup()
+
+
     return (
         <div className="order_card container">
             <h3><i className="fa-solid fa-cart-shopping shopping_cart"/> Order</h3>
-            <div>Placeholder order items go here</div>
+            {/* <div>{ordersMarkup}</div> */}
             <ul className="order_costs">
                 <li><span>Sub-total:</span><span>£0.00</span></li>
                 <li><span>Delivery fee:</span><span>£0.00</span></li>
